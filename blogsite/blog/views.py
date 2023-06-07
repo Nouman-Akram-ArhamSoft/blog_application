f8c0ab5ef07f8641ebbdba190932995c8f36aa7d
Nouman-Akram-ArhamSoft <nouman.akram@arhamsoft.org>
2023-06-07 12:36:39 +0500

    added reqirements.txt and comments model

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,47 +1,53 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url()
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommend you read " f"{post.title}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
-            subject = f"{cd['name']} recommend you read "\
-                f"{post.title}"
-            message = f"Read {post.title} at {post_url}\n\n"\
-                f"{cd['name']}\'s comments: {cd['comments']}"
 
             import os
-            print(os.environ.get("EMAIL_HOST_USER"), [cd['to']])
-            send_mail(subject, message, os.environ.get("EMAIL_HOST_USER"), [cd['to']])
+
+            print(os.environ.get("EMAIL_HOST_USER"), [cd["to"]])
+            send_mail(subject, message, os.environ.get("EMAIL_HOST_USER"), [cd["to"]])
 
             sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
 
 class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
+
 
 # def post_list(request):
 #     _post_list = Post.published.all()
@@ -65,16 +71,31 @@ class PostListView(ListView):
 
 
 def post_detail(request, year, month, day, post):
-
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
+        publish__day=day,
     )
-    return render(request,
-                  "blog/post/detail.html",
-                  {"post": post})
+    return render(request, "blog/post/detail.html", {"post": post})
 
+
+@require_POST
+def post_comment(request):
+    post = get_object_or_404(POST, id=id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
